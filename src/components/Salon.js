af2a21cb608e2085acf9ef614ba873f6fb0dff5f
nansiiii <nasrin.shakery8@gmail.com>
2023-07-22 23:42:32 +0330

    s

--- src/components/Salon.js
+++ src/components/Salon.js
@@ -3,7 +3,7 @@ import "./styles/style.scss";
 import axios from "axios";
 
 const Salon = () => {
-   // const [chairsData, setChairsData] = useState([]);
+   const [chairsData, setChairsData] = useState([]);
    const [sectionAchairsData, setSectionAchairsData] = useState([]);
    const [sectionBchairsData, setSectionBchairsData] = useState([]);
    const [sectionCchairsData, setSectionCchairsData] = useState([]);
@@ -13,13 +13,41 @@ const Salon = () => {
    useEffect(() => {
       axios.get("./chairs.json")
       .then((response)=> {
-         // setChairsData(response.data);
+         setChairsData(response.data);
          setSectionAchairsData(response.data.filter((chair) => chair.section === "A"))
          setSectionBchairsData(response.data.filter((chair) => chair.section === "B"))
          setSectionCchairsData(response.data.filter((chair) => chair.section === "C"))
          setSectionDchairsData(response.data.filter((chair) => chair.section === "D"))
       })
    }, [] );
+
+   const chairStateHandler = (event)=>{
+      // console.log(event.target.id);
+      let btnId = event.target.id;
+      let btn = event.target;
+      let state = event.target.value;
+      // setSectionAchairsData (...sectionAchairsData)
+      // let chair = chairsData.find((chair) =>chair.number === btn);
+      // let chair = sectionChairsData.find((chair) => chair.number == btn);
+      let chair = chairsData.find((chair) => chair.number == btnId);
+      console.log(chair);
+      console.log(state);
+      console.log(btn);
+      if ( state === "unselected"){
+         btn.style.backgroundColor = "green";
+         btn.style.color = "white";
+      }else if ( state === "selected"){
+         btn.style.backgroundColor = "orange";
+         btn.style.color = "white";
+         
+      }else if ( state === "reserving"){
+      
+      }else if ( state === "reserved"){
+
+      }
+      
+      
+   }
 
    return (
       <>
@@ -31,7 +59,14 @@ const Salon = () => {
                <div className="section sectionA">
                   {
                      sectionAchairsData.map((chair) =>(
-                        <button key={chair.number} title={chair.price}>{chair.number}</button>
+                        <button 
+                           key={chair.number}
+                           title={chair.price}
+                           id={chair.number}
+                           value = {chair.state}
+                           onClick={(event) => chairStateHandler(event)}
+                           >
+                              {chair.number}</button>
                      ))
                   }
                </div>
